1d1a157cae994407c058c76adeaa10bec881311a
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2023-09-20 09:02:27 -0300

    feature: add middleware for route product

--- api/routes/produtoRoute.js
+++ api/routes/produtoRoute.js
@@ -1,13 +1,28 @@
 const { Router } = require('express')
 const ProdutoController = require('../controllers/produtoController')
+const roles = require('../middleware/roles')
+const permissoes = require('../middleware/permissoes')
 
 const router = Router()
 
 router
-  .post('/produto', ProdutoController.cadastrarProduto)
-  .get('/produto', ProdutoController.buscarTodosProdutos)
-  .get('/produto/:id', ProdutoController.buscarProdutoPorId)
-  .delete('/produto/:id', ProdutoController.deletarProdutoPorId)
-  .put('/produto/:id', ProdutoController.editarProduto)
+  .post('/produto', roles(['Gerente']), ProdutoController.cadastrarProduto)
+  .get(
+    '/produto',
+    roles(['Gerente', 'Vendedor']),
+    ProdutoController.buscarTodosProdutos
+  )
+  .get(
+    '/produto/:id',
+
+    roles(['Gerente', 'Vendedor']),
+    ProdutoController.buscarProdutoPorId
+  )
+  .delete(
+    '/produto/:id',
+    roles(['Gerente']),
+    ProdutoController.deletarProdutoPorId
+  )
+  .put('/produto/:id', roles(['Gerente']), ProdutoController.editarProduto)
 
 module.exports = router
